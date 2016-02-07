3f9095c11eb61277f422eb47e164588b76bb9f1c
unknown <shahp2@SA2411-PC34.win.rpi.edu>
2016-02-07 16:22:43 -0500

    (Parsh) I started XML stuff, But we probably won't use it.

---
+++ holeSale/Assets/Scripts/Title.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Title : MonoBehaviour {
+    public string Name;
+    public float Price;
+    public float Discount;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ holeSale/Assets/Scripts/TitleManaager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Xml.Serialization;
+using System.Collections.Generic;
+using System.IO;
+
+[XmlRoot("TitleManager")]
+public class TitleManaager : MonoBehaviour {
+    [XmlArray("Titles")]
+    [XmlArrayItem("Title")]
+    public List<Title> Titles = new List<Title>();
+   // public Title[] Titles;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    public void Save(string path) {
+        var serializer = new XmlSerializer(typeof(TitleManaager));
+        using (var stream = new FileStream(path, FileMode.Create))
+        {
+            serializer.Serialize(stream, this);
+        }
+    
+    }
+
+    public static TitleManaager Load(string path)
+    {
+        
+    }
+}
