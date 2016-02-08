a6c1e159ec1aa5579a4d91d683b078ac58ee1be2
Hiz <leeh14@rpi.edu>
2016-02-07 23:13:47 -0500

    made collision work still need to work on sticking

--- holeSale/Assets/Scripts/GameTitle.cs
+++ holeSale/Assets/Scripts/GameTitle.cs
@@ -11,19 +11,28 @@ public class GameTitle : MonoBehaviour {
 
     void Awake()
     {
-        render = GetComponent<SpriteRenderer>();
-        if (Name == "Fallout")
-        {
-            render.sprite = Resources.Load<Sprite>("2D Assets/Titles 1_0");
-        }
-
+        //render = GetComponent<SpriteRenderer>();
+        //if (Name == "Fallout")
+        //{
+        //    render.sprite = Resources.Load<Sprite>("2D Assets/Titles 1_0");
+        //}
+        BoxCollider2D collision = gameObject.GetComponent<BoxCollider2D>();
+        collision.size = new Vector2(5.3f, 1.59f);
     }
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 
  
 	}
-	
+    void OnCollision2D(Collider2D coll)
+    {
+        Debug.Log("hit");
+    }
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        Debug.Log("hit2");
+        gameObject.transform.parent = coll.gameObject.transform;
+    }
 	// Update is called once per frame
 	void Update () {
 		transform.position += movedir * speed * Time.deltaTime;
